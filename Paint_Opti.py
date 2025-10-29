651258f3c91a6d4fe624d86264834cb50b5f2e4f
ZePytime <ZePytime@proton.me>
2025-10-29 17:09:58 +0100

    Update Paint_Opti.py

--- Paint_Opti.py
+++ Paint_Opti.py
@@ -4,105 +4,55 @@ from math import *
 from kandinsky import *
 from ion import *
 from time import *
-c=color
-f=fill_rect
-k=keydown
-o=sleep
-a=True
-b=0.01
-S=[320,222]
-z=255
-d=[c(z,z,z),c(0,0,0),c(z,0,0),c(z,z,0),c(0,z,0),c(0,z,z),c(z,0,z)]
-e=[]
-g=8
-h={KEY_ONE:1,KEY_TWO:2,KEY_THREE:3,KEY_FOUR:4,KEY_FIVE:5,KEY_SIX:6,KEY_SEVEN:7,KEY_EIGHT:8,KEY_NINE:9}
-i=[0,0]
-j=8
-l=c(0,0,0)
-class u:
-  def __init__(self,p,q,r):
-    self.p=p
-    self.ai=[p[0]+q[0],p[1]+q[1]]
-    self.q=q
-    self.r=r
-    f(self.p[0],self.p[1],self.q[0],self.q[1],self.r)
-  def s(self,v):
-    global j
-    aj,al=v[0],v[1]
-    aa,ab=v[0]+j,v[1]+j
-    ac,ad=self.p
-    ae,af=self.ai
-    ag=aj<ae and aa>ac
-    ah=al<af and ab>ad
-    return ag and ah
-def n(x,y):
-  global j
-  global i
-  global l
-  global m
-  if x>S[0]:
-    x=0
-  elif y>S[1]:
-    y=0
-  elif x<0-j:
-    x=S[0]
-  elif y<0-j:
-    y=S[1]
-  if not a:
-    f(i[0],i[1],j,j,m)
-    m=get_pixel(x,y)
-  for t in e:
-    if t.s([x,y]):
-      l=t.r
-      break
-  f(x,y,j,j,l)
-  if j>2:
-    f(x+1,y+1,j-2,j-2,c(130,130,130))
-  i=[x,y]
-  o(b)
-def ak():
-  global a
-  global j
-  global g
-  global b
-  global m
-  global h
-  global e
-  x=0
-  for r in d:
-    x+=10
-    e.append(u([x,5],[20,20],r))
-    x+=20
-  n(160,110)
-  while True:
-    if k(KEY_UP):
-      n(i[0],i[1]-1)
-    if k(KEY_DOWN):
-      n(i[0],i[1]+1)
-    if k(KEY_LEFT):
-      n(i[0]-1,i[1])
-    if k(KEY_RIGHT):
-      n(i[0]+1,i[1])
-    if k(KEY_PLUS):
-      o(0.1)
-      b-=0.001
-    if k(KEY_MINUS):
-      o(0.1)
-      b+=0.001
-    if k(KEY_OK):
-      o(0.25)
-      a=not a
-      if a:
-        j=g
-      else:
-        g=j
-        f(i[0],i[1],j,j,l)
-        m=l
-        j=1
-    if a:
-      for w in h.keys():
-        if k(w):
-          f(i[0],i[1],j,j,l)
-          j=h[w]
-          break
-ak()
+h=color
+w=keydown
+i=255
+j=150
+m=fill_rect
+S=[320,222];B=h(240,i,240);G=h(j,j,j)
+P=[h(i,i,i),h(0,0,0),h(i,0,0),h(i,i,0),h(0,i,0),h(0,i,i),h(i,0,i)]
+class A:
+ def __init__(s):s.a=True;s.b=0.01;s.j=8;s.g=8;s.i=[0,0];s.l=h(0,0,0);s.m=B;s.e=[]
+def f(xy,z,c):m(xy[0],xy[1],z,z,c)
+class U:
+ def __init__(s,p,q,r):s.p=p;s.q=[p[0]+q,p[1]+q];s.r=r;f(p,q,r)
+ def s(s,v,t):
+  x,y=v[0]+t.j//2,v[1]+t.j//2
+  return s.p[0]<=x<=s.q[0] and s.p[1]<=y<=s.q[1]
+def o(x,y,t):
+ if x>S[0]:x=0
+ elif y>S[1]:y=0
+ elif x<0-t.j:x=S[0]
+ elif y<0-t.j:y=S[1]
+ if not t.a:f(t.i,t.j,t.m);t.m=get_pixel(x,y)
+ for v in t.e:
+  if v.s([x,y],t):t.l=v.r;break
+ if t.a:
+  f([x,y],t.j,t.l)
+  if t.j>2:f([x+1,y+1],t.j-2,G)
+ else:f([x,y],t.j,G)
+ t.i=[x,y];sleep(t.b)
+def p(t):
+ x=0
+ for r in P:x+=10;t.e.append(U([x,5],20,r));x+=20
+ o(160,110,t)
+ while 1:
+  if w(KEY_UP):o(t.i[0],t.i[1]-1,t)
+  elif w(KEY_DOWN):o(t.i[0],t.i[1]+1,t)
+  if w(KEY_LEFT):o(t.i[0]-1,t.i[1],t)
+  elif w(KEY_RIGHT):o(t.i[0]+1,t.i[1],t)
+  if w(KEY_PLUS):sleep(0.05);t.b=max(0,t.b-0.002)
+  elif w(KEY_MINUS):sleep(0.05);t.b=min(0.2,t.b+0.002)
+  if w(KEY_OK):
+   sleep(0.25)
+   if t.a:
+    f(t.i,t.j,t.l);t.m=t.l;t.g=t.j;t.a=False;t.j=1
+   else:t.a=True;t.j=t.g
+  if t.a:
+   if w(KEY_MULTIPLICATION):
+    sleep(0.25);f(t.i,t.j,t.l);t.j=min(10,t.j+1);o(t.i[0],t.i[1],t)
+   elif w(KEY_DIVISION):
+    sleep(0.25);f(t.i,t.j,t.l);t.j=max(1,t.j-1);o(t.i[0],t.i[1],t)
+m(0,0,S[0],S[1],B)
+T=A()
+p(T)
